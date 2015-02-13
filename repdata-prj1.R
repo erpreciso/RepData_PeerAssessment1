2638f731477f52420632de5640b835f2b2fabc6a
erpreciso <trepreciso@gmail.com>
2015-02-13 13:40:24 +0100

    remove unused files

--- repdata-prj1.R
+++
@@ -1,9 +0,0 @@
-setwd("/home/erpreciso/Documents/school/repdata-prj1")
-if (!file.exists("raw")){
-    dir.create("raw")
-}
-inputUrl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
-temp <- tempfile()
-download.file(inputUrl, temp, method="curl")
-data <- read.csv(unz(temp, "activity.csv"))
-unlink(temp)
